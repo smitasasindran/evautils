5c4926d24084d183f89702806f4e8247b5e44c9a
smitasasindran <smita.sasindran@glassbeam.com>
2020-02-01 07:13:21 +0530

    Removing PIL from setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ VERSION = '0.1.0'
 # What packages are required for this module to be executed?
 REQUIRED = [
     # 'requests', 'maya', 'records',
-    'tensorflow', 'numpy', 'matplotlib', 'PIL'
+    'tensorflow', 'numpy', 'matplotlib'
 ]
 
 # What packages are optional?
